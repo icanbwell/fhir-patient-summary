acfe4b9b3bfbb25fce32e8704591edb7ba910ca6
Imran Qureshi <imranq2@hotmail.com>
2025-06-20 16:06:07 -0700

    expose classes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export const myPackage = (taco = ''): string => `${taco} from my package`;
+export { ComprehensiveIPSCompositionBuilder } from "./generators/fhir_summary_generator";
+export { NarrativeGenerator } from "./generators/narrative_generator";
