46ae7a45fc78d99638e75287c60259e25d2c034d
Imran Qureshi <imranq2@hotmail.com>
2025-12-12 01:36:53 -0800

    Add additional coding system display names for ICD-10-CM and IMO Problem IT

--- src/structures/codingSystemDisplayNames.ts
+++ src/structures/codingSystemDisplayNames.ts
@@ -5,13 +5,14 @@ const CODING_SYSTEM_DISPLAY_NAMES: Record<string, string> = {
   'http://snomed.info/sct': 'SNOMED CT',
   'http://loinc.org': 'LOINC',
   'http://hl7.org/fhir/sid/icd-10': 'ICD-10',
+  'http://hl7.org/fhir/sid/icd-10-cm': 'ICD-10-CM',
   'http://hl7.org/fhir/sid/icd-9': 'ICD-9',
   'http://hl7.org/fhir/sid/cvx': 'CVX',
   'http://www.nlm.nih.gov/research/umls/rxnorm': 'RxNorm',
   'http://www.ama-assn.org/go/cpt': 'CPT',
   'http://unitsofmeasure.org': 'UCUM',
+  'http://e-imo.com/products/problem-it': 'IMO Problem IT',
   // Add more as needed
 };
 
 export default CODING_SYSTEM_DISPLAY_NAMES;
-
