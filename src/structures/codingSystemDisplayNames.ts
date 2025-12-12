90d16f9cd8cf5c281991cc7126c60667e3f3c3bb
Imran Qureshi <imranq2@hotmail.com>
2025-12-12 05:48:06 -0800

    Update mapping for CDC to CVX in codingSystemDisplayNames

--- src/structures/codingSystemDisplayNames.ts
+++ src/structures/codingSystemDisplayNames.ts
@@ -15,7 +15,7 @@ const CODING_SYSTEM_DISPLAY_NAMES: Record<string, string> = {
   '2.16.840.1.113883.6.285': 'HCPCS Level II',
   'https://fhir.cerner.com/4ff3b259-e48d-4066-8b35-a6a051f2802a/codeSet/72': 'Cerner Code Set 72',
   'http://hl7.org/fhir/sid/ndc': 'NDC', // Added mapping for NDC
-  'urn:oid:2.16.840.1.113883.12.292': 'CDC',
+  'urn:oid:2.16.840.1.113883.12.292': 'CVX',
   'http://terminology.hl7.org/CodeSystem/data-absent-reason': 'Data Absent Reason', // Added mapping for Data Absent Reason
   '2.16.840.1.113883.6.208': 'NDDF',
   // Add more as needed
